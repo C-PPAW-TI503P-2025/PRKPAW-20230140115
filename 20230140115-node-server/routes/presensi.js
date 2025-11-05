f3a44eadc4ee1b1cdaad90d56aaa966bb3b7a42f
dhitozarfhan <dhitozarfhan@gmail.com>
2025-11-05 13:43:21 +0700

    tugas5

--- 20230140115-node-server/routes/presensi.js
+++ 20230140115-node-server/routes/presensi.js
@@ -7,5 +7,23 @@ router.use(addUserData);
 
 router.post('/check-in', presensiController.CheckIn);
 router.post('/check-out', presensiController.CheckOut);
+const { body } = require("express-validator");
+router.put(
+  "/:id",
+  [
+    body("checkIn")
+      .optional()
+      .isISO8601()
+      .withMessage("Format tanggal checkIn tidak valid. Gunakan format YYYY-MM-DD atau ISO."),
+    body("checkOut")
+      .optional()
+      .isISO8601()
+      .withMessage("Format tanggal checkOut tidak valid. Gunakan format YYYY-MM-DD atau ISO."),
+  ],
+  presensiController.updatePresensi
+);
+router.delete("/:id", presensiController.deletePresensi);
 
 module.exports = router;
+
+
